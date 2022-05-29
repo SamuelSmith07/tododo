52029e2d5a530c9a2adba64c2093a98b4958de55
SamuelSmith <samsmith1@live.com>
2022-05-29 22:14:17 +1000

    first

---
+++ src/components/InputField.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+    <form className='Input'><Input type="input" placeholder="Enter a tododo"></div>
+  )
+}
+
+export default InputField
